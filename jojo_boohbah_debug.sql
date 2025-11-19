a108acdf299cacb561e583bd71d47918d0deee67
smbrown22 <smbrown22@my.waketech.edu>
2025-11-19 08:03:06 -0500

    Add files via upload

--- jojo_boohbah_debug.sql
+++ jojo_boohbah_debug.sql
@@ -1,49 +1,52 @@
--- 1. 
+-- 1. readjusted  
 SELECT boohbah_id, (select AVG(sync_level) from boohbah_stand_link) as average, name
 FROM boohbah_stand_link
 JOIN boohbah USING (boohbah_id)
-order BY boohbah_id;  
--- 2.
+group by boohbah_id, name;  
+-- 2. readjusted  
 SELECT b.name, s.stand_name
 FROM boohbah b
 JOIN jojo_stand s
-ON b.boohbah_id = s.stand_id;
--- 3. 
+ON b.boohbah_id = s.stand_id
+JOIN boohbah_stand_link l ON b.boohbah_id = l.boohbah_id; 
+-- 3. correct 
 SELECT name, color
 FROM boohbah
 WHERE energy_level > 80; 
--- 4. 
+-- 4. correct 
 SELECT b.boohbah_id, s.stand_id
 FROM boohbah b, jojo_Stand s
 JOIN boohbah_stand_link l
 ON boohbah_id = l.boohbah_id
 WHERE b.boohbah_id = 2; 
--- 5. 
+-- 5. correct 
 SELECT b.name
 FROM boohbah b
 WHERE b.energy_level > (SELECT AVG(energy_level) FROM boohbah);
--- 6. 
-SELECT name
+-- 6. readjusted 
+SELECT name 
 FROM boohbah
-WHERE energy_level > all (SELECT power
+WHERE energy_level > (SELECT max(power)
                       FROM jojo_stand
                       WHERE season = 3);
--- 7. 
+-- 7. readjusted  
 SELECT b.name, s.stand_name
-FROM boohbah b, jojo_stand s
-WHERE b.energy_level > 80 and boohbah_id = stand_id;
--- 8. 
+FROM boohbah b
+JOIN boohbah_stand_link l ON b.boohbah_id = l.boohbah_id
+JOIN jojo_stand s ON l.stand_id = s.stand_id
+WHERE b.energy_level > 80;
+-- 8. correct 
 SELECT boohbah_id, sync_level
 FROM boohbah_stand_link
 WHERE sync_level > (select AVG(sync_level) from boohbah_stand_link); 
--- 9. 
+-- 9. correct 
 SELECT boohbah_id, stand_id
 FROM boohbah_stand_link
 WHERE (boohbah_id, stand_id) IN
       (SELECT boohbah_id, stand_id FROM boohbah_stand_link);
--- 10. 
+-- 10. adjust 
 MERGE INTO boohbah b
-USING jojo_stand s
-ON (b.boohbah_id = s.stand_id)
+USING boohbah_stand_link l
+ON (b.boohbah_id = l.stand_id)
 WHEN MATCHED THEN
   UPDATE SET b.energy_level = 999; 
